559b08f2c8b55e2d808a1577312a9b0741a5620c
Igor Degtyarenko <div50015@mail.ru>
2025-05-29 16:11:49 +0300

    add fill form, ball.jpg file and  assert

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,8 @@ from selenium import webdriver
 def browser_manager():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 2.0
-    browser.config.window_width = 1900
-    browser.config.window_height = 1080
-
+    browser.config.window_width = 900
+    browser.config.window_height = 600
     # вводим дпнные не посимвольно а строками с помощью JS
     # browser.config.type_by_js = True
 
@@ -19,11 +18,6 @@ def browser_manager():
     # закрываем сообщение что браузер запущен в отладочном режиме
     driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
     browser.config.driver_options = driver_options
-
-    # уменьшение масштаба страницы до 65%
-    # browser.driver.execute_script("document.querySelector('.body-height').style.transform='scale(.65)'")
-    # browser.execute_script('document.querySelector("#fixedban").remove()')
-    # browser.element('footer').execute_script('element.remove()')
 
     yield
 
--- tests/qaguru_form.py
+++ tests/qaguru_form.py
@@ -1,12 +1,18 @@
 import pytest
-from selene import browser, have
+from selene import browser, have, command
+from selenium.webdriver import Keys
 import time
+import os.path
+
+
 
 def test_registration_form():
     # Given
     browser.open('/automation-practice-form')
     browser.execute_script('document.querySelector("#fixedban").remove()') # delete element fixedban
     browser.element('footer').execute_script('element.remove()') # delete element footer
+    # browser.driver.execute_script("document.querySelector('.body-height').style.transform='scale(.65)'") # уменьшение масштаба страницы до 65%
+
 
     # When
     browser.element('#firstName').type('Igor')
@@ -14,7 +20,31 @@ def test_registration_form():
     browser.element('#userEmail').type('div@novoch.ru')
     browser.all('[for^=gender-radio]').element_by(have.text('Male')).click()
     browser.element('#userNumber').type('9185024041')
-
-    time.sleep(3)
+    browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL,'a').type('04 august 1967').press_enter()
+    browser.element('#subjectsInput').type('History').press_enter()
+    browser.element('[for=hobbies-checkbox-1]').perform(command.js.scroll_into_view) # прокрутка скрола до элемента
+    browser.element('[for=hobbies-checkbox-1]').click()
+    import tests
+    browser.element('#uploadPicture').send_keys(os.path.dirname(tests.__file__),'/files/ball.jpg')
+    browser.element('#currentAddress').type('Russian Novocherkassk')
+    browser.element('#state').click()
+    browser.all('[id^=react-select]').element_by(have.text('Rajasthan')).click()
+    browser.element('#city').click()
+    browser.all('[id^=react-select]').element_by(have.text('Jaipur')).click()
+    browser.element('#submit').click()
+    # time.sleep(3)
 
     #Then
+    browser.element('.table').all('td').should(have.texts(
+        'Student Name', 'Igor Degt',
+        'Student Email', 'div@novoch.ru',
+        'Gender', 'Male',
+        'Mobile', '9185024041',
+        'Date of Birth', '04 August,1967',
+        'Subjects', 'History',
+        'Hobbies', 'Sports',
+        'Picture', 'ball.jpg',
+        'Address', 'Russian Novocherkassk',
+        'State and City', 'Rajasthan Jaipur',
+        )
+    )
