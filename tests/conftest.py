f75f596b319ef911df5da985e9fd935cc3b373fc
Igor Degtyarenko <div50015@mail.ru>
2025-05-29 15:07:47 +0300

    edit conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,16 +10,18 @@ def browser_manager():
     browser.config.window_width = 1900
     browser.config.window_height = 1080
 
+    # вводим дпнные не посимвольно а строками с помощью JS
     # browser.config.type_by_js = True
-    #
-    # driver_options = webdriver.ChromeOptions()
-    # # driver_options.add_argument('--headless=new')
-    # driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    # browser.config.driver_options = driver_options
+
+    driver_options = webdriver.ChromeOptions()
+    # не выводим на экран браузер
+    # driver_options.add_argument('--headless=new')
+    # закрываем сообщение что браузер запущен в отладочном режиме
+    driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    browser.config.driver_options = driver_options
 
     # уменьшение масштаба страницы до 65%
     # browser.driver.execute_script("document.querySelector('.body-height').style.transform='scale(.65)'")
-    # закрываем сообщение что браузер запущен в отладочном режиме
     # browser.execute_script('document.querySelector("#fixedban").remove()')
     # browser.element('footer').execute_script('element.remove()')
 
